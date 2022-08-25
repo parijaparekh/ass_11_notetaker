972b4aff3041dc6adebe5266cc0cb16581f52781
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-08-25 18:37:42 +1000

    added week 3 thursday content and homework

---
+++ 03-JavaScript/03-Thursday/28-Stu_Mini-Project/Main/script.js
@@ -0,0 +1,19 @@
+// Global variables for the scores
+
+// Array of options for computer to pick from
+
+var playGame = function () {
+	// Ask user for their choice
+	// If user pressed Cancel, immediately end function
+	// Convert to uppercase to make comparisons easier
+	// Get random index from array of options
+	// If choices are the same, it's a tie
+	// Check every win condition for the player
+	// If above conditions failed, assume player lost
+	// Print stats with line breaks
+	// Ask user to play again
+	// If user pressed OK, run the function again
+};
+
+// Run the game for the first time
+playGame();
