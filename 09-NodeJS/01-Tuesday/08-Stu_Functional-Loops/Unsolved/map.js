d3d4b5cd38044b04e1964c5b784c729697d0a7d4
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-10-06 21:27:00 +1100

    added week 9 Thursday content

--- 09-NodeJS/01-Tuesday/08-Stu_Functional-Loops/Unsolved/map.js
+++ 09-NodeJS/01-Tuesday/08-Stu_Functional-Loops/Unsolved/map.js
@@ -6,12 +6,16 @@ const doubledArray = originalArray.map(function(data) {
   return data * 2;
 });
 
-console.log(doubledArray);
-console.log(originalArray);
+const doubledArray2 = originalArray.map((data) => data * 2);
+
+
+// console.log(doubledArray);
+// console.log(originalArray);
 
 
 // TODO: Describe how map is working in the example below. What will the value of tripledArray be?
 const tripledArray = originalArray.map(data => data * 3);
+console.log(tripledArray);
 
 // TODO: Describe how map is working in the example below. What will the value of oddOrEven be?
 const oddOrEven = originalArray.map(num => {
@@ -21,3 +25,5 @@ const oddOrEven = originalArray.map(num => {
     return 'odd'
   }
 });
+
+console.log(oddOrEven);
