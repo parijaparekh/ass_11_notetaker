d1713387134891dbe2a5be03d38c56f50f30efaa
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-10-14 10:57:58 +1100

    added solved version of Thursday content

--- 10-OOP/02-Thursday/20-Stu_Mock-Fs/Unsolved/test/fileIO.test.js
+++ 10-OOP/02-Thursday/20-Stu_Mock-Fs/Unsolved/test/fileIO.test.js
@@ -1,15 +1,34 @@
-const FileIO = require('../fileIO');
+const FileIO = require("../fileIO");
+const fs = require("fs");
 
-describe('FileIO', () => {
-  describe('read', () => {
-    it("should call fs.readFileSync with the passed in 'file' argument", () => {
-      // TODO: Your code here
-    });
-  });
+jest.mock("fs");
 
-  describe('write', () => {
-    it("should call fs.writeFileSync with the passed in 'path' and 'data' arguments", () => {
-      // TODO: Your code here
-    });
-  });
+describe("FileIO", () => {
+	describe("read", () => {
+		it("should call fs.readFileSync with the passed in 'file' argument", () => {
+			// TODO: Your code here
+			const fileIo = new FileIO();
+			const file = "message.txt";
+			let data;
+
+			fs.readFileSync.mockReturnValue("Hello World");
+			data = fileIo.read(file);
+
+			expect(data).toEqual("Hello World");
+			expect(fs.readFileSync).lastCalledWith(file, "utf8");
+		});
+	});
+
+	describe("write", () => {
+		it("should call fs.writeFileSync with the passed in 'path' and 'data' arguments", () => {
+			// TODO: Your code here
+			const fileIo = new FileIO();
+			const path = "message.txt";
+			const data = "Hello World";
+
+			fileIo.write(path, data);
+
+			expecet(fs.writeFileSync).lastCalledWith(path, data);
+		});
+	});
 });
