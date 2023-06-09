b9840f0e2a519e54779ce50d53f9b6a3631d14e5
Parija Parekh <parija.parekh@gmail.com>
2023-06-09 15:04:47 +0800

    removed some folders

--- 04-Web-APIs/03-Thursday/28-Stu_Mini-Project/Main/assets/js/script.js
+++
@@ -1,5 +0,0 @@
-var wordBlank = document.querySelector(".word-blanks");
-var win = document.querySelector(".win");
-var lose = document.querySelector(".lose");
-var timerElement = document.querySelector(".timer-count");
-var startButton = document.querySelector(".start-button");
